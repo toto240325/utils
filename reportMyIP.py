e1a62bc72ff8040a1dac6a0e82d2b9bfec085732
toto titi <toto240325@gmail.com>
2021-03-16 22:30:16 +0100

    .

---
+++ reportMyIP.py
@@ -0,0 +1,28 @@
+import socket
+import fcntl
+import struct
+import requests
+import socket
+
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+myIP = get_ip_address('wlan0')
+hostname = socket.gethostname()
+
+print("hostname : " + hostname)
+print("myIP : "+myIP)
+
+r = requests.get("http://192.168.0.147/monitor/getEvent.php?eventFct=add&host={0}&type=ip&text={1}".format(hostname,myIP))
+print(r.json())
+
+r = requests.get("http://192.168.0.147/monitor/getEvent.php?eventFct=getLastEventByType&host=hostname&type=ip")
+print(r.json())
+
+
